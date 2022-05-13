4cabc33c54b15ec471411620fc9684f02bf59a3a
bombi <bombic3@naver.com>
2022-05-13 17:47:26 +0900

    Add chap07_tireclass, training_SeasonRepeat, Square

---
+++ training/src/training/EnumScope.java
@@ -0,0 +1,20 @@
+package training;
+import java.util.Scanner;
+
+class EnumScope {
+	public static void main(String[] args) {
+		Scanner stdIn = new Scanner(System.in);
+		System.out.print("정수 A:"); int a = stdIn.nextInt();
+		System.out.print("정수 B:"); int b = stdIn.nextInt();
+		if(a > b) {
+			int t = a; // t 또는 temp (temporarily) - 임시로 쓰는 변수명)
+			a = b;
+			b = t;
+		}			//  a가 b보다 크면 두 수를 교환
+		do {
+			System.out.print(a + " ");
+			a = a +1;
+		} while (a <= b);
+		System.out.println();
+	}
+}
